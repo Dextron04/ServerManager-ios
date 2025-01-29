5a197dd70c68e90f134b08898b9993b771f27db5
Dextron <tueshin12@gmail.com>
2025-01-28 21:11:55 -0800

    Initial Commit

---
+++ ServerManager/ServerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerManagerApp.swift
+//  ServerManager
+//
+//  Created by Tushin Kulshreshtha on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ServerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ServerManagerTests/ServerManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ServerManagerTests.swift
+//  ServerManagerTests
+//
+//  Created by Tushin Kulshreshtha on 1/28/25.
+//
+
+import Testing
+@testable import ServerManager
+
+struct ServerManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
